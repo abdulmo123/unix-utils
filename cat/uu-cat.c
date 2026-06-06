a42b104e6abf0ef9e55f5c2bdfac9dc8ac75daac
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-06-06 17:01:17 -0500

    add ability for cat tool to read from stdin

--- cat/uu-cat.c
+++ cat/uu-cat.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-
-	return 0;
+	if (argc > 1) {
+		//for (int i = 0; i < argc; i++)  {
+		//	printf("argv[%d] = %s\n", i, argv[i]);
+		//}
+		char *filename = argv[1];
+		FILE *fp = fopen(filename, "r");
+		if (fp == NULL) {
+			fp = stdin;
+		}
+		//printf("I'm in there dawg!\n");
+		char line[256];
+		while (fgets(line, sizeof(line), fp)) {
+			printf("%s", line);
+		}
+		fclose(fp);
+	}
 }
