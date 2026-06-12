e5df573b7c44efd797da6cd8fb1ac2e0c7f2d674
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-06-12 11:46:03 -0500

    support -n and -c args

--- head/uu-head.c
+++ head/uu-head.c
@@ -19,34 +19,28 @@ int main(int argc, char *argv[]) {
 		char files[FILES][FILENAME_LENGTH];
 		for (int i = 1; i < argc; i++)  {
 			char *str = argv[i];
-			// printf("str = %s\n", str);
 			if (strstr(argv[i], ".txt") != NULL) {
 				strcpy(files[f], argv[i]);
 				f++;
 			}
 			if (startsWith(argv[i], "-")) {
-				// printf("size of argv[i] == %d\n", strlen(argv[i]));
 				dash_arg[0] = argv[i][1];
 				dash_arg[1] = '\0';
 
 				
 				if (strlen(argv[i]) > 2) {
 					str = &argv[i][2];
-					// printf("str is here !!! %s\n", str);
 					while ((*str >= '0') && (*str <= '9')) {
 						len = (len * 10) + ((*str) - '0');
 						str++;
 					}	
 				} 
-                // printf("dash_arg[1] = %d\n", dash_arg[1]);
 			} 
 
 			while ((*str >= '0') && (*str <= '9')) {
 				len = (len * 10) + ((*str) - '0');
 				str++;
 			}
-
-			// printf("len = %d\n", len);
 		}
 		execute_head_cmd(f, len, files, dash_arg);
 	} else {
@@ -70,15 +64,13 @@ bool startsWith(const char *str, const char *prefix) {
 }
 
 void execute_head_cmd(int f, int len, char files[FILES][FILENAME_LENGTH], char dash_arg[4]) {
-    printf("files size ... %d\n", f);
-    printf("dash arg ... %s \n", dash_arg);
-	printf("len = %d\n", len);
-	printf("===================================================\n");
-	int n = 0;
-
 	FILE *fp = NULL;
 	for (int i = 0; i < f; i++) {
-		// printf("files[%d] = %s\n", i, files[i]);
+		int c = 0;
+		int n = 0;
+		if (f > 1) {
+			printf("==> %s <==\n", files[i]);
+		}
 		fp = fopen(files[i], "r");
 		if (strcmp(dash_arg, "n") == 0) {
 			char line[1024];
@@ -86,6 +78,13 @@ void execute_head_cmd(int f, int len, char files[FILES][FILENAME_LENGTH], char d
 				printf("%s", line);
 				n++;
 			}
+		} else if (strcmp(dash_arg, "c") == 0) {
+			int ch;
+			while ((ch = fgetc(fp)) != EOF && c < len) {
+				printf("%c", ch);
+				c++;
+			}
+			printf("\n");
 		}
 		fclose(fp);
 	}
